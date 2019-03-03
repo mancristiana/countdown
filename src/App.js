488b25b28d5cb3f3dac92ed08ce6e156db48349f
Cristiana Man <man.cristiana1@gmail.com>
2019-03-03 18:09:48 +0100

    Use Unsplash API

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { getRandomPhoto } from './Unsplash.service';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      image: '',
+      color: '#000000'
+    };
+  }
+
+  componentDidMount() {
+    getRandomPhoto('home').then(({ urls, color }) => {
+      this.setState({ image: urls.full, color });
+    });
+  }
+
+  getContrast(hexcolor) {
+    const r = parseInt(hexcolor.substr(1, 2), 16);
+    const g = parseInt(hexcolor.substr(3, 2), 16);
+    const b = parseInt(hexcolor.substr(5, 2), 16);
+    const yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq >= 128 ? 'black' : 'white';
+  }
+
   render() {
+    const { image, color } = this.state;
+    const headerStyle = {
+      backgroundColor: `${color}7F`
+    };
+    const textStyle = {
+      color: this.getContrast(color)
+    };
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+        <header className="App-header" style={headerStyle}>
+          <img src={image} className="App-image" alt="logo" />
+          <p className="App-text" style={textStyle}>
+            <span className="number">33</span>remaining until moving in.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     );
