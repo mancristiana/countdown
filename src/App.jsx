ed53c99200b611f61456fa27c62f0f5437f17cda
Cristiana Man <man.cristiana1@gmail.com>
2019-03-09 20:51:10 +0100

    Cleanup App css and markup

--- src/App.jsx
+++ src/App.jsx
@@ -4,27 +4,14 @@ import './App.css';
 import Counter from './components/counter/Counter';
 import RandomImage from './components/randomImage/RandomImage';
 
-import { getContrast } from './shared/Color.service';
-
 const App = () => {
   const [color, setColor] = useState('#fff');
-
-  const headerStyle = {
-    backgroundColor: `${color}7F`
-  };
-
-  const textStyle = {
-    color: getContrast(color)
-  };
-
   return (
     <div className="App">
-      <header className="App-header" style={headerStyle}>
-        <RandomImage setColor={color => setColor(color)} />
-        <div className="App-text" style={textStyle}>
-          <Counter endDate={'2019-06-01'} />
-        </div>
-      </header>
+      <RandomImage setColor={color => setColor(color)} />
+      <div className="App-counter">
+        <Counter endDate={'2019-06-01'} color={color} />
+      </div>
     </div>
   );
 };
