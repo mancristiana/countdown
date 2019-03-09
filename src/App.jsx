3c0d6843a437106187745421f250665d3432f4bf
Cristiana Man <man.cristiana1@gmail.com>
2019-03-09 20:42:24 +0100

    Make app a functional component and use hooks

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Counter from './components/counter/Counter';
@@ -6,38 +6,27 @@ import RandomImage from './components/randomImage/RandomImage';
 
 import { getContrast } from './shared/Color.service';
 
-class App extends PureComponent {
-  constructor(props) {
-    super(props);
-    this.state = {
-      color: '#fff'
-    };
-  }
+const App = () => {
+  const [color, setColor] = useState('#fff');
 
-  setColor = color => {
-    this.setState(color);
+  const headerStyle = {
+    backgroundColor: `${color}7F`
   };
 
-  render() {
-    console.log('RENDERING APP');
-    const { color } = this.state;
-    const headerStyle = {
-      backgroundColor: `${color}7F`
-    };
-    const textStyle = {
-      color: getContrast(color)
-    };
-    return (
-      <div className="App">
-        <header className="App-header" style={headerStyle}>
-          <RandomImage setColor={color => console.log(color)} />
-          <div className="App-text" style={textStyle}>
-            <Counter endDate={'2019-06-01'} />
-          </div>
-        </header>
-      </div>
-    );
-  }
-}
+  const textStyle = {
+    color: getContrast(color)
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header" style={headerStyle}>
+        <RandomImage setColor={color => setColor(color)} />
+        <div className="App-text" style={textStyle}>
+          <Counter endDate={'2019-06-01'} />
+        </div>
+      </header>
+    </div>
+  );
+};
 
 export default App;
