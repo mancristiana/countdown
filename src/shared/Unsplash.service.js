169cab0502cd23fd4090f89472659e7736584b1b
Cristiana Man <man.cristiana1@gmail.com>
2019-03-09 22:46:33 +0100

    Remove Unsplash mock function

--- src/shared/Unsplash.service.js
+++ src/shared/Unsplash.service.js
@@ -5,10 +5,9 @@ const api = axios.create({
   headers: { Authorization: 'Client-ID 042454a26c390216eda97edd0c96560ecb88b5753a79bb740dc8283b7a7d665b' }
 });
 
-export async function getRandomPhoto2(query) {
+export async function getRandomPhoto(query) {
   try {
     const response = await api.get(`/photos/random?query=${query}`);
-    console.log(response.data);
     return response.data;
   } catch (error) {
     console.error(error);
@@ -19,18 +18,4 @@ export async function getRandomPhoto2(query) {
       }
     };
   }
-}
-
-export async function getRandomPhoto(query) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      console.log('this was called');
-      resolve({
-        color: '#CACEE3',
-        urls: {
-          full: 'https://images.unsplash.com/photo-1493606371202-6275828f90f3?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&ixid=eyJhcHBfaWQiOjU4MTUwfQ'
-        }
-      });
-    }, 100);
-  });
 }
