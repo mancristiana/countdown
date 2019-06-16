23536ac1af5759053d4161a8c7b66eb5224fc516
Cristiana Man <man.cristiana1@gmail.com>
2019-06-16 19:14:15 +0200

    Updates

--- precache-manifest.72cc6e4fe783267589c78c2b7fb482cc.js
+++ precache-manifest.72cc6e4fe783267589c78c2b7fb482cc.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "beae622ea37fb91d67b1",
+    "revision": "9b2bf1eae153ae1bd11e",
     "url": "/countdown/static/css/main.7e1e46b1.chunk.css"
   },
   {
-    "revision": "beae622ea37fb91d67b1",
-    "url": "/countdown/static/js/main.beae622e.chunk.js"
+    "revision": "9b2bf1eae153ae1bd11e",
+    "url": "/countdown/static/js/main.9b2bf1ea.chunk.js"
   },
   {
     "revision": "2dba6604ba83043e2dd6",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/countdown/static/js/2.1c46bef2.chunk.js"
   },
   {
-    "revision": "d681ee0a1e80b3ec6986bea08d92d9f7",
+    "revision": "29de9df500299660d48f578a09c8645c",
     "url": "/countdown/index.html"
   }
 ];
