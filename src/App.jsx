d1a5b404ebb624872af8cd2245644b7db43a76b8
Cristiana Man <cma@templafy.com>
2019-08-05 11:03:49 +0200

    Make a Templafy Countdown

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import React, { useState } from 'react';
 import styles from './App.module.css';
 
 import Counter from './components/counter/Counter';
-import RandomImage from './components/randomImage/RandomImage';
+// import RandomImage from './components/randomImage/RandomImage';
+import Image from './components/image/Image';
 import Header from './components/header/Header';
 
 import { ColorContext } from './shared/Color.context';
 
 const App = () => {
-  const [color, setColor] = useState('#fff');
+  const [color, setColor] = useState('#000');
   return (
     <div className={styles.app}>
       <ColorContext.Provider value={{ color, setColor }}>
-        <RandomImage />
+        <Image />
         <div className={styles.counter}>
-          <Header>Christmas in</Header>
-          <Counter endDate={'2019-12-24'} />
+          {/* <Header>Templafy Ignite</Header> */}
+          <Counter endDate={'2019-08-06 09:00'} />
         </div>
       </ColorContext.Provider>
     </div>
