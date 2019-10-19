8ab0bd4d21bd9019eb571655c39ce47fb42bbb5a
Cristiana Man <man.cristiana1@gmail.com>
2019-10-19 23:06:13 +0200

    Revert "Make a Templafy Countdown"
    This reverts commit d1a5b404ebb624872af8cd2245644b7db43a76b8.

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,20 @@ import React, { useState } from 'react';
 import styles from './App.module.css';
 
 import Counter from './components/counter/Counter';
-// import RandomImage from './components/randomImage/RandomImage';
-import Image from './components/image/Image';
+import RandomImage from './components/randomImage/RandomImage';
 import Header from './components/header/Header';
 
 import { ColorContext } from './shared/Color.context';
 
 const App = () => {
-  const [color, setColor] = useState('#000');
+  const [color, setColor] = useState('#fff');
   return (
     <div className={styles.app}>
       <ColorContext.Provider value={{ color, setColor }}>
-        <Image />
+        <RandomImage />
         <div className={styles.counter}>
-          {/* <Header>Templafy Ignite</Header> */}
-          <Counter endDate={'2019-08-06 09:00'} />
+          <Header>Christmas in</Header>
+          <Counter endDate={'2019-12-24'} />
         </div>
       </ColorContext.Provider>
     </div>
--- src/components/image/Image.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import styles from './Image.module.css';
-import image from './../../bg.png';
-
-const Image = () => {
-  return <img src={image} className={styles.image} alt="logo" />;
-};
-
-export default Image;
