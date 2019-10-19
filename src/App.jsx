4ec4751f38a1969bde5630ce4a4107d704d125bd
Cristiana Man <man.cristiana1@gmail.com>
2019-10-19 23:15:46 +0200

    Change background random image query to winter. Prettify

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import React, { useState } from 'react';
-import styles from './App.module.css';
+import React, { useState } from "react";
+import styles from "./App.module.css";
 
-import Counter from './components/counter/Counter';
-import RandomImage from './components/randomImage/RandomImage';
-import Header from './components/header/Header';
+import Counter from "./components/counter/Counter";
+import RandomImage from "./components/randomImage/RandomImage";
+import Header from "./components/header/Header";
 
-import { ColorContext } from './shared/Color.context';
+import { ColorContext } from "./shared/Color.context";
 
 const App = () => {
-  const [color, setColor] = useState('#fff');
+  const [color, setColor] = useState("#fff");
   return (
     <div className={styles.app}>
       <ColorContext.Provider value={{ color, setColor }}>
-        <RandomImage />
+        <RandomImage query={"winter"} />
         <div className={styles.counter}>
           <Header>Christmas in</Header>
-          <Counter endDate={'2019-12-24'} />
+          <Counter endDate={"2019-12-24"} />
         </div>
       </ColorContext.Provider>
     </div>
--- src/components/randomImage/RandomImage.jsx
+++ src/components/randomImage/RandomImage.jsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect, useContext } from 'react';
-import styles from './RandomImage.module.css';
+import React, { useState, useEffect, useContext } from "react";
+import styles from "./RandomImage.module.css";
 
-import { getRandomPhoto } from '../../shared/Unsplash.service';
-import { ColorContext } from '../../shared/Color.context';
+import { getRandomPhoto } from "../../shared/Unsplash.service";
+import { ColorContext } from "../../shared/Color.context";
 
-const RandomImage = () => {
-  const [image, setImage] = useState('');
+const RandomImage = ({ query }) => {
+  const [image, setImage] = useState("");
   const { setColor } = useContext(ColorContext);
   useEffect(() => {
-    getRandomPhoto('home').then(({ urls, color }) => {
+    getRandomPhoto(query).then(({ urls, color }) => {
       setImage(urls.full);
       setColor(color);
     });
